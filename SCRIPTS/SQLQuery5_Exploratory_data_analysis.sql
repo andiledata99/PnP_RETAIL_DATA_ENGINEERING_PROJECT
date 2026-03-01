773485962ea4470177a6ce4921407a287c90f32f
Andile Dube <andile.data99@gmail.com>
2026-03-01 17:21:11 +0200

    pushing data exploration code using the terminal

---
+++ SCRIPTS/SQLQuery5_Exploratory_data_analysis.sql
@@ -0,0 +1,11 @@
+---#Exploratory data analysis to iderntify trends in the data 
+
+--Finding out how many   cashier employees in total the store have.
+SELECT DISTINCT Cashiername 
+FROM master_table_2025_pnp_sales;
+
+--Finding out how many stores the company has?
+SELECT DISTINCT Storename
+FROM master_table_2025_pnp_sales;
+
+--
